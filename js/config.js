7ee2852caa0855a917ff43c74279377598aae96a
DEV-qs <qingshui9362@gmail.com>
2026-07-05 11:31:13 +0800

    update

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   API_BASE_URL:
     window.location.hostname === "localhost"
       ? ""
-      : "https://luno-dashboard-proxy.qingshui9362.workers.dev/ms-my/",
+      : "https://luno-dashboard-proxy.qingshui9362.workers.dev/ms-my",
 
   STORAGE_KEYS: {
     API_KEY: "luno_api_key",
